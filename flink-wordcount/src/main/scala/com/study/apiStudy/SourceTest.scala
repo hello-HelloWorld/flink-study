6561833a12892fc12b69fb4f8967d3d571f84ebd
sunxiaoxiong <sunxiaoxiong@xhxdata.com>
2020-07-21 17:55:59 +0800

    添加flink的数据来源和转换算子

--- flink-wordcount/src/main/scala/com/study/apiStudy/SourceTest.scala
+++ flink-wordcount/src/main/scala/com/study/apiStudy/SourceTest.scala
@@ -1,6 +1,15 @@
 package com.study.apiStudy
 
 import java.sql.Timestamp
+import java.util.Properties
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+
+import scala.collection.immutable
+import scala.util.Random
 
 /*
 * 数据来源：
@@ -14,5 +23,83 @@ import java.sql.Timestamp
 case class SensorReading(id: String, timestamp: Long, temperature: Double)
 
 object SourceTest {
+  def main(args: Array[String]): Unit = {
+    //创建执行环境
+    val environment: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    //设置并行度
+    environment.setParallelism(1)
 
+    //1.从集合中读取数据
+    val stream1: DataStream[SensorReading] = environment.fromCollection(List(
+      SensorReading("sensor_1", 1547718199, 35.8),
+      SensorReading("sensor_6", 1547718201, 15.4),
+      SensorReading("sensor_7", 1547718202, 6.7),
+      SensorReading("sensor_10", 1547718205, 38.1),
+      SensorReading("sensor_1", 1547718207, 37.2),
+      SensorReading("sensor_1", 1547718212, 33.5),
+      SensorReading("sensor_1", 1547718215, 38.1)
+    ))
+    //    stream1.print("stream1").setParallelism(2)
+
+    val stream2: DataStream[Any] = environment.fromElements(1, 2.2, "aa") //可以放不同类型的数据
+    //    stream2.print("stream2")
+
+    //2.从文件中读取数据
+    val stream3: DataStream[String] = environment.readTextFile("E:\\workspace\\flink-study\\flink-wordcount\\src\\main\\resources\\sensor.txt")
+    //    stream3.print("stream3").setParallelism(2)
+
+    //3.读取socket文本流
+    val stream4: DataStream[String] = environment.socketTextStream("hadoop102", 7777)
+
+    //4.从kafka中读取数据
+    val properties: Properties = new Properties()
+    properties.setProperty("bootstrap.servers", "hadoop104:9092")
+    //properties.setProperty("group.id", "ab")
+    properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    properties.setProperty("auto.offset.reset", "latest")
+    val stream5: DataStream[String] = environment.addSource(new FlinkKafkaConsumer011[String]("topicb", new SimpleStringSchema(), properties))
+
+    //5.自定义source
+    val stream6: DataStream[SensorReading] = environment.addSource(new MySensorSource())
+    stream6.print("stream6")
+
+    environment.execute("source test job")
+  }
+}
+
+// 实现一个自定义的 SourceFunction，自动生成测试数据
+class MySensorSource() extends SourceFunction[SensorReading] {
+  //定义一个flag，表示数据源是否正常运行
+  var running: Boolean = false
+
+  override def cancel(): Unit = running = false
+
+  //随机生成SensorReading数据
+  override def run(sourceContext: SourceFunction.SourceContext[SensorReading]): Unit = {
+    //定义一个随机数生成器
+    val random: Random = new Random()
+    // 随机生成 10个传感器的温度值，并且不停在之前温度基础上更新（随机上下波动）
+    // 首先生成 10个传感器的初始温度
+    var curtmps: immutable.IndexedSeq[(String, Double)] = 1.to(10).map(
+      i => ("sensor_" + i, 60 + random.nextGaussian() * 20)
+    )
+
+    //无限循环，生成随机数流
+    while (true) {
+      //在当前温度基础上，随机生成小微动
+      curtmps = curtmps.map(
+        data => (data._1, data._2 + random.nextGaussian())
+      )
+
+      //获取当前系统时间
+      val curTs: Long = System.currentTimeMillis()
+      //包装成样例类，用sourceContext发出数据
+      curtmps.foreach(
+        data => sourceContext.collect(SensorReading(data._1, curTs, data._2))
+      )
+      //定义时间间隔
+      Thread.sleep(1000)
+    }
+  }
 }
---
+++ flink-wordcount/src/main/scala/com/study/apiStudy/TransformTest.scala
@@ -0,0 +1,91 @@
+package com.study.apiStudy
+
+import org.apache.flink.api.common.functions.{MapFunction, ReduceFunction}
+import org.apache.flink.api.java.functions.KeySelector
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.streaming.api.scala._
+
+/*
+* @author: helloWorld
+* @date  : Created in 2020/7/21 16:07
+*/
+
+//转换算子
+object TransformTest {
+  def main(args: Array[String]): Unit = {
+    //创建执行环境
+    val environment: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    //设置并发度
+    environment.setParallelism(1)
+    //读取数据
+    val inputData: DataStream[String] = environment.readTextFile("E:\\workspace\\flink-study\\flink-wordcount\\src\\main\\resources\\sensor.txt")
+
+    //1.基本转换操作
+    val stream1: DataStream[SensorReading] = inputData.map(data => {
+      val dataArr: Array[String] = data.split(",")
+      SensorReading(dataArr(0), dataArr(1).toLong, dataArr(2).toDouble)
+    })
+    //    stream1.print("基本转换操作").setParallelism(2)
+
+    //2.分组滚动聚合
+    val stream2 = stream1.
+      keyBy("id")
+      //        .keyBy(data=>data.id)
+      //      .keyBy(new MyIdSelector())
+      //      .min("temperature") //取当前sensor温度最小的值
+      //      .reduce(new MyReducer())//取出每个sensor最大的时间戳和最小的温度
+      .reduce((curData, newData) => SensorReading(curData.id, curData.timestamp.max(newData.timestamp), curData.temperature.min(newData.temperature)))
+    //        stream2.print()
+
+    //3.分流,split和select一起用
+    val splitStream: SplitStream[SensorReading] = stream1.split(data => {
+      if (data.temperature > 38) {
+        Seq("high")
+      } else {
+        Seq("low")
+      }
+    })
+    val highStream: DataStream[SensorReading] = splitStream.select("high")
+    val lowStream: DataStream[SensorReading] = splitStream.select("low")
+    val allStream: DataStream[SensorReading] = splitStream.select("low", "high")
+    /*  stream1.print()
+      highStream.print("high")
+      lowStream.print("low")
+      allStream.print("all")*/
+
+    //4.合流，connect和mao一起使用
+    val waringStream: DataStream[(String, Double)] = highStream.map(
+            data => (data.id, data.temperature)
+//      new MyMapper()
+    )
+    val connectedStreams: ConnectedStreams[SensorReading, (String, Double)] = lowStream.connect(waringStream)
+
+  val resultStream: DataStream[Object] = connectedStreams.map(
+    lowTempData => (lowTempData.id, "normal"),
+      warningData => (warningData._1, warningData._2, "high temp warning")
+  )
+    //    resultStream.print()
+
+    //使用union进行合并
+    val value: DataStream[SensorReading] = highStream.union(lowStream,allStream)
+    value.print()
+
+
+    environment.execute()
+  }
+}
+
+// 自定义函数类，key选择器
+class MyIdSelector() extends KeySelector[SensorReading, String] {
+  override def getKey(in: SensorReading): String = in.id
+}
+
+class MyReducer() extends ReduceFunction[SensorReading] {
+  override def reduce(t: SensorReading, t1: SensorReading): SensorReading = {
+    SensorReading(t.id, t.timestamp.max(t1.timestamp), t.temperature.min(t1.timestamp))
+  }
+}
+
+class MyMapper extends MapFunction[SensorReading, (String, Double)]() {
+  override def map(t: SensorReading): (String, Double) = (t.id, t.temperature)
+}
